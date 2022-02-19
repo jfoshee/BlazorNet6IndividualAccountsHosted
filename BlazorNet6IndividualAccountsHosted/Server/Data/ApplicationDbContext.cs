fcf24dc8cda61356f1e42dad2b4d7f753b6740b0
Jacob Foshee <jacobf@gmail.com>
2022-02-19 10:54:02 -0600

    Add project files.

---
+++ BlazorNet6IndividualAccountsHosted/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorNet6IndividualAccountsHosted.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorNet6IndividualAccountsHosted.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ BlazorNet6IndividualAccountsHosted/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorNet6IndividualAccountsHosted.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
